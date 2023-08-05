bc1ebc21afbf4f63ccd31268b245956291b47a25
shajan1234 <shajansantosh123@gmail.com>
2023-08-05 09:20:59 +0530

    Add files via upload

---
+++ bar_code/lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:simple_barcode_scanner/simple_barcode_scanner.dart';
+import 'package:url_launcher/url_launcher.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String result = '';
+
+  Future _launchUrl() async {
+    if (result == '') {
+      return;
+    }
+    final Uri url = Uri.parse(result);
+    if (await launchUrl(url)) {
+      print('URL Launched');
+    }
+    else{
+      print('Failed to Launch');
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ElevatedButton(
+              onPressed: () async {
+                var res = await Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SimpleBarcodeScannerPage(),
+                    ));
+                setState(() {
+                  if (res is String) {
+                    result = res;
+                  }
+                });
+              },
+              child: const Text('Open Scanner'),
+            ),
+            Text('Barcode Result: $result'),
+            ElevatedButton(onPressed: _launchUrl, child: const Text('open URL'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
